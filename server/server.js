c35051c20185e475fd090b242dfa15fd0f2753ce
arash <arash.asnaashari@gmail.com>
2020-11-02 19:22:15 +0330

    authstuff

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 const schema = require("./graphql/schema");
 const rootValue = require("./graphql/resolves");
 
@@ -10,6 +10,18 @@ const isAuth = require("./middleware/is-auth");
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(isAuth);
 mongoose
   .connect(
